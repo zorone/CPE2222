31b72276fde34f76eb579a8592a69002bad19cd1
Imron  Jehleh <imron.jehleh@gmail.com>
2023-08-29 22:38:34 +0700

    Fixed incorrectly justify text.

--- Assignment 3.py
+++ Assignment 3.py
@@ -19,11 +19,11 @@ Amount.append(int(input("Enter Quantity of Product:")))
 print(pageBreak)
 print("Inventory".center(50))
 print(pageBreak)
-print("Item".center(30), "Price".center(10), "Quantity".center(10))
+print("{I}{P}{A}".format(I="Item".center(30), P="Price".center(10), A="Quantity".center(10)))
 print(pageBreak)
 
 for i in range(3):
-    print("{I}{P}{A}".format(I=Item[i].ljust(30), P=Price[i].center(7), A=Amount[i].rjust(13)))
+    print("{I}{P}{A}".format(I=Item[i].ljust(30), P=str(Price[i]).center(7), A=str(Amount[i]).rjust(13)))
     Total += Amount[i]
 
 print(pageBreak)
