0e97eec98400747096aca6eaaa9da6796389860a
Imron  Jehleh <imron.jehleh@gmail.com>
2023-08-29 23:29:09 +0700

    Init assignment 4.py

---
+++ Assignment 4.py
@@ -0,0 +1,18 @@
+s1 = "Python is a powerful high-level, object-oriented programming language created by
+Guido van Rossum."
+s2 = "It has simple easy-to-use syntax, making it the perfect language for someone trying to
+learn computer programming for the first time."
+s3 = "Professionally, Python is great for backend web development, data analysis, artificial
+intelligence, and scientix computing."
+
+length = s1.len() + s2.len() + s3.len()
+wCount = s1.split().len() + s2.split().len() + s3.split().len()
+
+s2m = s2.strip
+s3m = s3.strip
+
+secret = s1[-21] + s1[-4] + s1[-82] s2m[44] + s2m[4] + s3m[-32] + s3m[-10]
+
+print("Total characters in string s1, s2 and s3 are {} characters".format(length))
+print("Total words in string s1, s2 and s3 are {} words".format(wCount))
+print("The secret code is {}".format(secret))
