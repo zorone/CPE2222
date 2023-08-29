9b7846bafaa6c8e6f1e8378935487fc1803d3a0d
Imron  Jehleh <imron.jehleh@gmail.com>
2023-08-29 23:33:14 +0700

    Fixed missing character.

--- Assignment 4.py
+++ Assignment 4.py
@@ -8,7 +8,7 @@ wCount = s1.split().len() + s2.split().len() + s3.split().len()
 s2m = s2.strip
 s3m = s3.strip
 
-secret = s1[-21] + s1[-4] + s1[-82] s2m[44] + s2m[4] + s3m[-32] + s3m[-10]
+secret = s1[-21] + s1[-4] + s1[-82] + s2m[44] + s2m[4] + s3m[-32] + s3m[-10]
 
 print("Total characters in string s1, s2 and s3 are {} characters".format(length))
 print("Total words in string s1, s2 and s3 are {} words".format(wCount))
