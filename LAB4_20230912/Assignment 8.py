fe31bdcccf3f7ce99ba41aa9b593149b8a504e4f
Imron jehleh <imron.jehleh@gmail.com>
2023-09-12 04:08:28 +0700

    Shorten Source Codes

--- LAB4_20230912/Assignment 8.py
+++ LAB4_20230912/Assignment 8.py
@@ -19,58 +19,32 @@ s_ratio.sort()
 temp = int()
 
 char_point = tuple()
-temp = point.index(s_point[-1])
-char_point += (letter[temp], )
-point[temp] = -1
-temp = point.index(s_point[-2])
-char_point += (letter[temp], )
-point[temp] = -1
-temp = point.index(s_point[-3])
-char_point += (letter[temp], )
-point[temp] = -1
-temp = point.index(s_point[-4])
-char_point += (letter[temp], )
-point[temp] = -1
-
 char_amount = tuple()
-temp = amount.index(s_amount[-1])
-char_amount += (letter[temp], )
-amount[temp] = -1
-temp = amount.index(s_amount[-2])
-char_amount += (letter[temp], )
-amount[temp] = -1
-temp = amount.index(s_amount[-3])
-char_amount += (letter[temp], )
-amount[temp] = -1
-temp = amount.index(s_amount[-4])
-char_amount += (letter[temp], )
-amount[temp] = -1
-
 char_ratio = tuple()
-temp = ratio.index(s_ratio[0])
-char_ratio += (letter[temp], )
-ratio[temp] = -1
-temp = ratio.index(s_ratio[1])
-char_ratio += (letter[temp], )
-ratio[temp] = -1
-temp = ratio.index(s_ratio[2])
-char_ratio += (letter[temp], )
-ratio[temp] = -1
-temp = ratio.index(s_ratio[3])
-char_ratio += (letter[temp], )
-ratio[temp] = -1
+
+for i in range(-1, -5, -1):
+    temp = point.index(s_point[i])
+    char_point += (letter[temp], )
+    point[temp] = -1
+    
+    temp = amount.index(s_amount[i])
+    char_amount += (letter[temp], )
+    amount[temp] = -1
+    
+    temp = ratio.index(s_ratio[(i*-1)-1])
+    char_ratio += (letter[temp], )
+    ratio[temp] = -1
+
 
 print("The highest point in the scrabble game:")
-print('        1) "{}" with {} points'.format(char_point[0], s_point[-1]))
-print('        2) "{}" with {} points'.format(char_point[1], s_point[-2]))
-print('        3) "{}" with {} points'.format(char_point[2], s_point[-3]))
-print('        4) "{}" with {} points'.format(char_point[3], s_point[-4]))
+
+for i in range(1, 5):
+    print('        i) "{}" with {} points'.format(char_point[i-1], s_point[i*-1]))
 
 print("The highest amount in the scrabble game:")
-print('        1) "{}" with {} pieces'.format(char_amount[0], s_amount[-1]))
-print('        2) "{}" with {} pieces'.format(char_amount[1], s_amount[-2]))
-print('        3) "{}" with {} pieces'.format(char_amount[2], s_amount[-3]))
-print('        4) "{}" with {} pieces'.format(char_amount[3], s_amount[-4]))
+
+for i in range(1, 5):
+    print('        i) "{}" with {} pieces'.format(char_amount[i-1], s_amount[i*-1]))
 
 print("The lowest ratio in the scrabble game:")
 print('        1) "{}" with {} percent'.format(char_ratio[0], s_ratio[0]))
