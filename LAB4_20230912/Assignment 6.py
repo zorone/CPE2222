0fff5726d4e51e21abfa0cafee855cb5f50c95ba
Imron  Jehleh <imron.jehleh@gmail.com>
2023-09-11 19:48:39 +0700

    Complete dictionary.

--- LAB4_20230912/Assignment 6.py
+++ LAB4_20230912/Assignment 6.py
@@ -1,9 +1,31 @@
-dictionary = {"index": [1, 2, 3, 4],
-              "Name": {1: "Peter", 2: "Paul", 3: "Mary", 4: "Jenny"},
-              "Age": {1: 40, 2: 25, 3: 18, 4: 60},
+dictionary = {"index": [0, 1, 2, 3],
+              "Name": ["Peter", "Paul", "Mary", "Jenny"],
+              "Age": [40, 25, 18, 60],
+              "Gender": ["Male", "Male", "Female", "Female"],
               "Test": {
                   {"First": [20, 19, 10, 5]},
                   {"Second": [18, 20, 5, 3]},
                   {"Third": [19, 19, 4, 1]}
                   }
               }
+
+print("\"{}\" is {}".format(dictionary["Name"][0], dictionary["Gender"][0]))
+
+print("The 1st test score of \"{}\" is {}".format(dictionary["Name"][2], dictionary["Test"]["First"][2]))
+
+print("The 2nd test score of \"{}\" is {}".format(dictionary["Name"][3], dictionary["Test"]["Second"][3]))
+
+print("The 3rd test score of \"{}\" is {}".format(dictionary["Name"][1], dictionary["Test"]["Third"][1]))
+
+dictionary["index"].append(4)
+dictionary["Name"].append("Robert")
+dictionary["Age"].append(35)
+dictionary["Gender"].append("Male")
+dictionary["Test"]["First"].append(10)
+dictionary["Test"]["Second"].append(18)
+dictionary["Test"]["Third"].append(5)
+
+print("\"{}\" is {} years old".format(dictionary["Name"][4], dictionary["Age"][4]))
+
+print("The dictionary to solve this problem was designed as: ")
+print(dictionary)
