758b9d215acb0c10b597e68bb9718795da10d419
Imron  Jehleh <imron.jehleh@gmail.com>
2023-09-12 01:58:00 +0700

    Fixed wrong index called.

--- LAB4_20230912/Assignment 7.py
+++ LAB4_20230912/Assignment 7.py
@@ -39,22 +39,32 @@ AtA = multiplyMatrix(A, tA)
 starBreak = '*' * 90
 pageBreak = '-' * 90
 
+print(A)
+print(tA)
+print(AtA)
+
 print(starBreak)
 print("Matrix with Tuple".center(90))
 print(starBreak)
 
 x = int(input('Enter row number of the "A" matrix: '))
+x -= 1
 y = int(input('Enter column number of the "A" matrix: '))
+y -= 1
 print('The "a{x}{y}" element in the "A" matrix is {n}'.format(x=x, y=y, n=A[x][y]))
 
 print(pageBreak)
 x = int(input('Enter row number of the transposed "A" matrix: '))
+x -= 1
 y = int(input('Enter column number of the transposed "A" matrix: '))
+y -= 1
 print('The "b{x}{y}" element in the transposed "A" matrix is {n}'.format(x=x, y=y, n=tA[x][y]))
 
 print(pageBreak)
 x = int(input('Enter row number of multiplication of matrices "A" and transposed of "A": '))
+x -= 1
 y = int(input('Enter column number of multiplication of matrices "A" and transposed of "A": '))
+y -= 1
 print('The "c{x}{y}" element in multiplication of the "A" matrix and the transposed "A" matrix is {n}'.format(x=x, y=y, n=AtA[x][y]))
 
 print("-".ljust(90, "-"))
