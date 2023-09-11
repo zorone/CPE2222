462861146f0c6d17e20c776abb913d3cbc1c1987
Imron jehleh <imron.jehleh@gmail.com>
2023-09-12 03:49:09 +0700

    Fully implementation, still have to fixed some.

--- LAB4_20230912/Assignment 8.py
+++ LAB4_20230912/Assignment 8.py
@@ -8,9 +8,13 @@ point = scrabble[1::4]
 amount = scrabble[2::4]
 ratio = scrabble[3::4]
 
-s_point = point.sort()
-s_amount = amount.sort()
-s_ratio = ratio.sort()
+s_point = point.copy()
+s_amount = amount.copy()
+s_ratio = ratio.copy()
+
+s_point.sort()
+s_amount.sort()
+s_ratio.sort()
 
 char_point = tuple()
 char_point += (letter[point.index(s_point[-1])], )
@@ -25,6 +29,25 @@ char_amount += (letter[amount.index(s_amount[-3])], )
 char_amount += (letter[amount.index(s_amount[-4])], )
 
 char_ratio = tuple()
+char_ratio += (letter[ratio.index(s_ratio[0])], )
+char_ratio += (letter[ratio.index(s_ratio[1])], )
+char_ratio += (letter[ratio.index(s_ratio[2])], )
+char_ratio += (letter[ratio.index(s_ratio[3])], )
 
 print("The highest point in the scrabble game:")
-print('        1) "{}" with {} points'.format())
+print('        1) "{}" with {} points'.format(char_point[0], s_point[-1]))
+print('        2) "{}" with {} points'.format(char_point[1], s_point[-2]))
+print('        3) "{}" with {} points'.format(char_point[2], s_point[-3]))
+print('        4) "{}" with {} points'.format(char_point[3], s_point[-4]))
+
+print("The highest amount in the scrabble game:")
+print('        1) "{}" with {} points'.format(char_amount[0], s_amount[-1]))
+print('        2) "{}" with {} points'.format(char_amount[1], s_amount[-2]))
+print('        3) "{}" with {} points'.format(char_amount[2], s_amount[-3]))
+print('        4) "{}" with {} points'.format(char_amount[3], s_amount[-4]))
+
+print("The lowest ratio in the scrabble game:")
+print('        1) "{}" with {} points'.format(char_ratio[0], s_ratio[0]))
+print('        2) "{}" with {} points'.format(char_ratio[1], s_ratio[1]))
+print('        3) "{}" with {} points'.format(char_ratio[2], s_ratio[2]))
+print('        4) "{}" with {} points'.format(char_ratio[3], s_ratio[3]))
