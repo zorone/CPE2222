640b10d2b98954ece3943afa8670acae6d3cb788
Imron jehleh <imron.jehleh@gmail.com>
2023-09-12 04:00:14 +0700

    Fixed characters appear the same if value is the same.

--- LAB4_20230912/Assignment 8.py
+++ LAB4_20230912/Assignment 8.py
@@ -16,23 +16,49 @@ s_point.sort()
 s_amount.sort()
 s_ratio.sort()
 
+temp = int()
+
 char_point = tuple()
-char_point += (letter[point.index(s_point[-1])], )
-char_point += (letter[point.index(s_point[-2])], )
-char_point += (letter[point.index(s_point[-3])], )
-char_point += (letter[point.index(s_point[-4])], )
+temp = point.index(s_point[-1])
+char_point += (letter[temp], )
+point[temp] = -1
+temp = point.index(s_point[-2])
+char_point += (letter[temp], )
+point[temp] = -1
+temp = point.index(s_point[-3])
+char_point += (letter[temp], )
+point[temp] = -1
+temp = point.index(s_point[-4])
+char_point += (letter[temp], )
+point[temp] = -1
 
 char_amount = tuple()
-char_amount += (letter[amount.index(s_amount[-1])], )
-char_amount += (letter[amount.index(s_amount[-2])], )
-char_amount += (letter[amount.index(s_amount[-3])], )
-char_amount += (letter[amount.index(s_amount[-4])], )
+temp = amount.index(s_amount[-1])
+char_amount += (letter[temp], )
+amount[temp] = -1
+temp = amount.index(s_amount[-2])
+char_amount += (letter[temp], )
+amount[temp] = -1
+temp = amount.index(s_amount[-3])
+char_amount += (letter[temp], )
+amount[temp] = -1
+temp = amount.index(s_amount[-4])
+char_amount += (letter[temp], )
+amount[temp] = -1
 
 char_ratio = tuple()
-char_ratio += (letter[ratio.index(s_ratio[0])], )
-char_ratio += (letter[ratio.index(s_ratio[1])], )
-char_ratio += (letter[ratio.index(s_ratio[2])], )
-char_ratio += (letter[ratio.index(s_ratio[3])], )
+temp = ratio.index(s_ratio[0])
+char_ratio += (letter[temp], )
+ratio[temp] = -1
+temp = ratio.index(s_ratio[1])
+char_ratio += (letter[temp], )
+ratio[temp] = -1
+temp = ratio.index(s_ratio[2])
+char_ratio += (letter[temp], )
+ratio[temp] = -1
+temp = ratio.index(s_ratio[3])
+char_ratio += (letter[temp], )
+ratio[temp] = -1
 
 print("The highest point in the scrabble game:")
 print('        1) "{}" with {} points'.format(char_point[0], s_point[-1]))
@@ -41,13 +67,13 @@ print('        3) "{}" with {} points'.format(char_point[2], s_point[-3]))
 print('        4) "{}" with {} points'.format(char_point[3], s_point[-4]))
 
 print("The highest amount in the scrabble game:")
-print('        1) "{}" with {} points'.format(char_amount[0], s_amount[-1]))
-print('        2) "{}" with {} points'.format(char_amount[1], s_amount[-2]))
-print('        3) "{}" with {} points'.format(char_amount[2], s_amount[-3]))
-print('        4) "{}" with {} points'.format(char_amount[3], s_amount[-4]))
+print('        1) "{}" with {} pieces'.format(char_amount[0], s_amount[-1]))
+print('        2) "{}" with {} pieces'.format(char_amount[1], s_amount[-2]))
+print('        3) "{}" with {} pieces'.format(char_amount[2], s_amount[-3]))
+print('        4) "{}" with {} pieces'.format(char_amount[3], s_amount[-4]))
 
 print("The lowest ratio in the scrabble game:")
-print('        1) "{}" with {} points'.format(char_ratio[0], s_ratio[0]))
-print('        2) "{}" with {} points'.format(char_ratio[1], s_ratio[1]))
-print('        3) "{}" with {} points'.format(char_ratio[2], s_ratio[2]))
-print('        4) "{}" with {} points'.format(char_ratio[3], s_ratio[3]))
+print('        1) "{}" with {} percent'.format(char_ratio[0], s_ratio[0]))
+print('        2) "{}" with {} percent'.format(char_ratio[1], s_ratio[1]))
+print('        3) "{}" with {} percent'.format(char_ratio[2], s_ratio[2]))
+print('        4) "{}" with {} percent'.format(char_ratio[3], s_ratio[3]))
