74e0acf9c420234bad57724369c79da5cec815f5
Imron  Jehleh <imron.jehleh@gmail.com>
2023-09-11 19:32:51 +0700

    Implement dictionary more.

--- LAB4_20230912/Assignment 6.py
+++ LAB4_20230912/Assignment 6.py
@@ -1,2 +1,9 @@
-dictionary = dict()
-dictionary.setdefault("Peter", "{'Age': 40, 'Gender': 'Male', 'Test': {'First': 20, 'Second': 18, 'Third': 19} }")
+dictionary = {"index": [1, 2, 3, 4],
+              "Name": {1: "Peter", 2: "Paul", 3: "Mary", 4: "Jenny"},
+              "Age": {1: 40, 2: 25, 3: 18, 4: 60},
+              "Test": {
+                  {"First": [20, 19, 10, 5]},
+                  {"Second": [18, 20, 5, 3]},
+                  {"Third": [19, 19, 4, 1]}
+                  }
+              }
