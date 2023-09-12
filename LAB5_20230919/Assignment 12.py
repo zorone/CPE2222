fbe6e49942c3b29af9dfaee3ad0ae827ac271637
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 06:27:42 +0700

    Initialize assignment 12

---
+++ LAB5_20230919/Assignment 12.py
@@ -0,0 +1,25 @@
+acceptRange = range(1, 40)
+s = str()
+
+while True:
+    x = int(input("Enter a number for roman number conversion:"))
+    
+    if x not in acceptRange: break
+    
+    res, mod = divmod(x, 10)
+    s += "X"*res
+    
+    if(mod == 9):
+        s += "IX"
+        
+    elif(mod == 4):
+        s += "IV"
+        
+    else:
+        res, mod = divmod(x, 5)
+        s += "V"*res
+        s += "I"*mod
+
+    print('The roman number of {} is " {} "'.format(x, s))
+    
+    
