4952b478341f09717fd1163602eb7e0218bb880e
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 05:11:51 +0700

    Implement more...

--- LAB5_20230919/Assignment 10.py
+++ LAB5_20230919/Assignment 10.py
@@ -1,15 +1,24 @@
+import sys
+
 password = str((input("Setting your password:")))
 
-tokenNegative = (" ", " not ")
+tokenNegative = (" contain ", " not contain ")
 tokenAmount = ("at least ", "more than ")
+tokenType = ("capital ", "lowercase ", "8 ", "16 ")
 tokenQuantifier = ("letter", "number", "characters")
 header = "!!!ERROR!!! The password must"
+s = str()
 
 l = len(password)
 
 if l < 8:
-    s = header+tokenNegative[1]+"contain "+tokenAmount[0]+"8"+tokenQuantifier[2]
+    s = header+tokenNegative[1]+tokenAmount[0]+tokenType[2]+tokenQuantifier[2]
+elif l > 16:
+    s = header+tokenNegative[1]+tokenAmount[0]+tokenType[2]+tokenQuantifier[2]
+    
+if len(s) > 0:
     print(s)
+    sys.exit
 
 for c in password:
     
