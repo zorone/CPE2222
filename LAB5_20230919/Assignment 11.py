6b6cdfecb83d3d3c8424751ccc14d3d0c2a8679d
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 06:11:50 +0700

    Fixed initialize arr.

--- LAB5_20230919/Assignment 11.py
+++ LAB5_20230919/Assignment 11.py
@@ -1,8 +1,6 @@
 print("Making a list of Factorial series of n")
 n = int(input("Enter 'n' of Factorial number:"))
-arr = list()
-
-arr[0] = 1
+arr = [1]
 
 for i in range(2, n+1):
     arr += [arr[i-1] * i]
