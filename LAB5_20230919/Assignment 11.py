8741167d62f12318b707d7cf4b5ef98d757358d1
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 06:14:58 +0700

    Now it works.

--- LAB5_20230919/Assignment 11.py
+++ LAB5_20230919/Assignment 11.py
@@ -3,6 +3,6 @@ n = int(input("Enter 'n' of Factorial number:"))
 arr = [1]
 
 for i in range(2, n+1):
-    arr += [arr[i-1] * i]
+    arr += [arr[i-2] * i]
 
 print("A list of Factorial series of {} is {}".format(n, arr))
