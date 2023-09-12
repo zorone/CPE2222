4d93a499aa75ce01307bd91f04e49f8bf4ea63e4
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 06:45:54 +0700

    Try reduce complexity of assignment 10 codes.

--- LAB5_20230919/Assignment 10.py
+++ LAB5_20230919/Assignment 10.py
@@ -9,41 +9,23 @@ tokenType = ("capital ", "lowercase ", "8 ", "16 ")
 tokenQuantifier = ("letter", "number", "characters")
 header = "!!!ERROR!!! The password must"
 
-upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowerCase = "abcdefghijklmnopqrstuvwxyz"
-number = "0123456789"
-
-checkers = list("000")
-
-s = str()
+upperCase = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+lowerCase = set("abcdefghijklmnopqrstuvwxyz")
+number = set("0123456789")
 
 l = len(password)
+
+c = set(password)
 
 if l < 8:
     s = header+tokenNegative[0]+tokenAmount[0]+tokenArticle[1]+tokenType[2]+tokenQuantifier[2]
 elif l > 16:
     s = header+tokenNegative[1]+tokenAmount[1]+tokenArticle[1]+tokenType[3]+tokenQuantifier[2]
-    
-if len(s) > 0:
-    print(s)
-    sys.exit(0)
-
-for c in password:
-    if(checkers[0] == "0"): 
-        if c in upperCase:
-            checkers[0] = "1"
-    if(checkers[1] == "0"):
-        if c in lowerCase:
-            checkers[1] = "1"
-    if(checkers[2] == "0"):
-        if c in number:
-            checkers[2] = "1"
-
-if(checkers[0] == "0"):
+elif c not in upperCase:
     s = header+tokenNegative[0]+tokenAmount[0]+tokenArticle[0]+tokenType[0]+tokenQuantifier[0]
-elif(checkers[1] == "0"):
+elif c not in lowerCase:
     s = header+tokenNegative[0]+tokenAmount[0]+tokenArticle[0]+tokenType[1]+tokenQuantifier[0]
-elif(checkers[2] == "0"):
+elif c not in number:
     s = header+tokenNegative[0]+tokenAmount[0]+tokenArticle[0]+tokenQuantifier[1]
 else:
     s= ":-) Your password is correct (-:"
