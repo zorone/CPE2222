48bf62ea90b22555bec7a22ff90ecdb7a1ee0ffa
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 06:09:23 +0700

    Initialize assignment 11

---
+++ LAB5_20230919/Assignment 11.py
@@ -0,0 +1,10 @@
+print("Making a list of Factorial series of n")
+n = int(input("Enter 'n' of Factorial number:"))
+arr = list()
+
+arr[0] = 1
+
+for i in range(2, n+1):
+    arr += arr[i-1] * i
+
+print("A list of Factorial series of {} is {}".format(n, arr))
