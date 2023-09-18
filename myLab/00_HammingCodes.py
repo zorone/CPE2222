71c3238ec627de452dbb5e184a30b988385160ed
Imron jehleh <imron.jehleh@gmail.com>
2023-09-18 15:50:27 +0700

    Retry.

--- myLab/00_HammingCodes.py
+++ myLab/00_HammingCodes.py
@@ -1,29 +1,28 @@
 import numpy as np
 
 np.random.randint(0, 2, 16)
-# array([1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0])
+# array([1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1])
 
 bits = np.random.randint(0, 2, 16)
 
 enumerate(bits)
-# <enumerate at 0x1c2f9470700>
-
+# <enumerate at 0x25452421100>
 list(enumerate(bits))
 '''
 [(0, 1),
- (1, 1),
- (2, 0),
+ (1, 0),
+ (2, 1),
  (3, 0),
  (4, 0),
  (5, 1),
  (6, 0),
- (7, 0),
- (8, 1),
+ (7, 1),
+ (8, 0),
  (9, 1),
  (10, 0),
- (11, 0),
- (12, 1),
- (13, 0),
+ (11, 1),
+ (12, 0),
+ (13, 1),
  (14, 1),
  (15, 0)]
 '''
@@ -32,67 +31,38 @@ list(enumerate(bits))
 # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 [i for i, bit in enumerate(bits) if bit]
-# [0, 1, 5, 8, 9, 12, 14]
+# [0, 2, 5, 7, 9, 11, 13, 14]
 
 bits
-# array([1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0])
+# array([1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0])
 
-0 ^ 1 ^ 5 ^ 8 ^ 9 ^ 12 ^ 14
-# 7
+0 ^ 2 ^ 5 ^ 7 ^ 9 ^ 11 ^ 13 ^ 14
+# 1
 
 from functools import reduce
 import operator as op
 
 reduce(op.xor, [i for i, bit in enumerate(bits) if bit])
-# 7
+# 1
 
 reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])
-# 7
+# 1
 
-bin(7)
-# '0b111'
+bin(1)
+# '0b1'
 
-bits[8] = not bits[8]
 bits[1] = not bits[1]
 
 reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])
-# 14
+# 0
 
 bits
-# array([1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0])
+# array([1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0])
 
 bits[10] = not bits[10]
 reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])
-# 4
+# 10
 
 bits[10] = not bits[10]
 reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])
-# 14
-
-_ih
-'''
-['',
- 'import numpy as np',
- 'np.random.randint(0, 2, 16)',
- 'bits = np.random.randint(0, 2, 16)',
- 'enumerate(bits)',
- 'list(enumerate(bits))',
- '[i for i, bit in enumerate(bits)]',
- '[i for i, bit in enumerate(bits) if bit]',
- 'bits',
- '0 ^ 1 ^ 5 ^ 8 ^ 9 ^ 12 ^ 14',
- 'from functools import reduce',
- 'import operator as op',
- 'reduce(op.xor, [i for i, bit in enumerate(bits) if bit])',
- 'reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])',
- 'bin(7)',
- 'bits[8] = not bits[8]',
- 'bits[1] = not bits[1]',
- 'reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])',
- 'bits',
- 'bits[10] = not bits[10]',
- 'reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])',
- 'bits[10] = not bits[10]',
- 'reduce(lambda x, y: x ^ y, [i for i, bit in enumerate(bits) if bit])',
- '_ih']
-'''
+# 0
