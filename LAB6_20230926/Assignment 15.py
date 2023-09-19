997aacd3e52dfc718eb858a0bd0795b45d1dadea
Imron jehleh <imron.jehleh@gmail.com>
2023-09-19 19:00:05 +0700

    Initialize assignment 15

---
+++ LAB6_20230926/Assignment 15.py
@@ -0,0 +1,19 @@
+plaintext = input("Enter your plaintext (Alphabet Only):")
+secretkey = int(input("Enter your secret key (Number Only):"))
+
+def encrypt(P,k):
+    
+    
+
+
+
+def decrypt(C,k):
+    
+    
+    
+    
+    
+ciphertext = encrypt(plaintext,secretkey)
+print('The encrypted ciphertext:',ciphertext,sep='')
+decryptedtext = decrypt(ciphertext,secretkey)
+print('The decryption results:',decryptedtext,sep='')
