5c648d72168f6cb8de27d1f790824ff5ed5a4a83
Imron jehleh <imron.jehleh@gmail.com>
2023-09-19 19:07:45 +0700

    Further Implement codes.

--- LAB6_20230926/Assignment 15.py
+++ LAB6_20230926/Assignment 15.py
@@ -1,12 +1,15 @@
 plaintext = input("Enter your plaintext (Alphabet Only):")
 secretkey = int(input("Enter your secret key (Number Only):"))
 
+upperList = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+lowerList = tuple('abcdefghijklmnopqrstuvwxyz')
+
 def encrypt(P,k):
+    k %= 26
+    if(P in upperList):
+        temp = upperList.index(P)
+        return upperList[(temp+k)%26]
     
-    
-
-
-
 def decrypt(C,k):
     
     
