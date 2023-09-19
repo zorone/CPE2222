6bf3e42c39c4bf06d660a4858be7c30573270ef7
Imron jehleh <imron.jehleh@gmail.com>
2023-09-19 18:37:45 +0700

    More implementation.

--- LAB6_20230926/Assignment 14.py
+++ LAB6_20230926/Assignment 14.py
@@ -2,6 +2,9 @@ def nthDegreePascal(re :int, *listVal):
     if(re <= 1):
         pascalList = [1, 1][0:re+1]
         return pascalList
+    else:
+        pascalList = nthDegreePascal(re-1, listVal)
     
-    nthDegreePascal(re-1, listVal)
+    for i in range(0, len(pascalList)):
+        
     
