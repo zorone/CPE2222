847458b048516d1f7ec4a01b6521baee5ad77615
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 15:06:33 +0700

    Oops...

--- test01_20231017/exam01.py
+++ test01_20231017/exam01.py
@@ -1,16 +1,20 @@
 def recursive(size, *arr):
-    if(size == 0):
-        arr += (1, )
+    if(size>2):
+        recursive(size-1, arr)
+        sz = len(arr)
+        val = arr[sz-3] + 2*arr[sz-2] + 4*arr[sz-1]
+    elif(size == 2):
+        recursive(size-1, arr)
+        arr += (3, )
+    elif(size == 1):
+        recursive(size-1, arr)
+        arr += (2, )
     else:
-        if(size == 1):
-            arr += (2, )
-        elif(size == 2):
-            arr += (3, )
-        else:
-            size = len(arr)
-            val = arr[size-3] + 2*arr[size-2] + 4*arr[size-1]
-            arr += (val, )
-    
+        arr = (1, )
+
     return arr
 
+arr = tuple()
+
 n = int(input("Enter 'n' of Integer Sequence:"))
+print("A tuple of Integer Sequence [n={}] is {}".format(n, recursive(n, arr)))
