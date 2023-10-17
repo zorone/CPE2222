c97755693f28a92ce0037357cd7201289ff0a803
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 13:38:27 +0700

    Try to extract key from nested dictionary.

--- test01_20231017/exam07.py
+++ test01_20231017/exam07.py
@@ -1,6 +1,10 @@
 import module_exam as ext
 
 studentID = ext.Info_Dict.keys()
+availableSubject = ext.Info_Dict.values()
+availableSubject = availableSubject.keys()
+
+topic = str(input("Enter your subject {}".format(availableSubject)))
 
 for ID in studentID:
     print(ID)
