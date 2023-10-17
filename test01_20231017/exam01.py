0ac9dc26d7b57792532999c7df62661daa1fe135
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 14:57:28 +0700

    Initialize exaxm01

---
+++ test01_20231017/exam01.py
@@ -0,0 +1,15 @@
+def recursive(size, *arr):
+    if(size == 0):
+        arr += (1, )
+    elif(size == 1):
+        arr += (2, )
+    elif(size == 2):
+        arr += (3, )
+    else:
+        size = len(arr)
+        val = arr[size-3] + 2*arr[size-2] + 4*arr[size-1]
+        arr += (val, )
+    
+    return arr
+
+n = int(input("Enter 'n' of Integer Sequence:"))
