40f47103c8448c431523ebbc1c4a6cc69ed3fcc1
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 16:27:23 +0700

    I want to raise now. So tired.

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -1,14 +1,11 @@
 n = int(input("Please Enter Degree: "))
-arr = list(list())
 
-for i in range(0, n+2):
-    if(i == 0):
-        arr.insert(1, (1, ))
-    elif(i == 1):
-        arr.insert(1, (1, 1))
-    else:
-        arr.insert(i+2, (1, ))
-        for j in range(len(arr[i+1])-1):
-            val = arr[i+1][j] + arr[i+1][j+1]
-            arr[i+2] += (val,)
-    print(arr[i])
+arr = [1]
+
+for i in range(0, n):
+    for j in range(0, n+1):
+        if(j == 0):
+            sz = len(arr)
+            arr[j] = arr[sz-1]
+        
+        
