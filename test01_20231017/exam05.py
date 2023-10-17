e5dd7a034c69b6d995ba71ef86dcb10893d3cd0c
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 15:44:18 +0700

    Initialize exam05

---
+++ test01_20231017/exam05.py
@@ -0,0 +1,31 @@
+pageBreak = "*"*35
+
+print(pageBreak)
+price = int(input("The total price of products: "))
+pay = int(input("Customer payment: "))
+print(pageBreak)
+
+moneySize = (500, 100, 50, 20, 10, 5, 2, 1)
+
+if(price == pay):
+    print("Complete payment")
+elif(price > pay):
+    print("!!! Incorrect payment !!!")
+else:
+    print("List of money return")
+    
+    receipt = pay - price
+    tempLeft = 0
+    tempAmount = 0
+    
+    for size in moneySize:
+        tempAmount = receipt/size
+        if size > 10:
+            text1 = "Banknote"
+            text2 = "Piece"
+        else:
+            text1 = "Coin"
+            text2 = "Coin"
+        if tempAmount:
+            tempLeft = receipt%size
+            print("The {}-$Baht {} = {} {}(s)".format(size, text1, tempAmount, text2))
