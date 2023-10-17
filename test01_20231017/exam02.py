3dff2d9a8e1fb71ec196b8c5fa8edd30d6c67f08
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 15:28:27 +0700

    Oops, forgot.

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -5,9 +5,10 @@ print("-"*50)
 n = int(input("Enter the height of right triangle:"))
 
 while n != 0:
-    print("*".rjust(n))
+    if n > 1:
+        print("*".rjust(n))
     for i in range(1, n-1):
-        print("*".rjust(n-i), "*".rjust(n))
+        print("*".rjust(n-i), "*")
     print("*"*n)
     
     n = int(input("Enter the height of right triangle:"))
