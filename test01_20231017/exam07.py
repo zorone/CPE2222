8aca66a029ee521d6f41abecf86b78e89f679b79
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 13:44:26 +0700

    Gotcha

--- test01_20231017/exam07.py
+++ test01_20231017/exam07.py
@@ -1,9 +1,8 @@
 import module_exam as ext
 
 studentID = ext.Info_Dict.keys()
-availableSubject = ext.Info_Dict[0].keys()
-
-topic = str(input("Enter your subject {}".format(availableSubject)))
 
 for ID in studentID:
-    print(ID)
+    availableSubject = ext.Info_Dict[ID].keys()
+
+topic = str(input("Enter your subject {}".format(availableSubject)))
