5008431ef4a4ebb32c70a7ef2a77b53ee7a268db
Imron jehleh <imron.jehleh@gmail.com>
2023-10-17 16:19:02 +0700

    Works a bit.

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -1,14 +1,14 @@
 n = int(input("Please Enter Degree: "))
-arr = list()
+arr = list(list())
 
 for i in range(0, n+2):
     if(i == 0):
-        arr[0] = ([1],)
+        arr.insert(1, (1, ))
     elif(i == 1):
-        arr[1] = ([1, 1],)
+        arr.insert(1, (1, 1))
     else:
-        arr[i+2] = [1]
+        arr.insert(i+2, (1, ))
         for j in range(len(arr[i+1])-1):
             val = arr[i+1][j] + arr[i+1][j+1]
-            arr[i+2] += ([val],)
+            arr[i+2] += (val,)
     print(arr[i])
