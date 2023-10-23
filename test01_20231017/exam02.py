b1b5b81df4bcbf657d58da2c21e399ec30749127
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 18:04:42 +0700

    stuck at implement nested list.

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -4,12 +4,13 @@ arr = [1]
 arrList = list()
 arrList += arr
 
-for i in range(1, n):
+for i in range(1, n+1):
     arr = list()
     arr += 1
+    sz = len(arr)
     for j in range(0, n):
-        if(j == 0):
-            sz = len(arr)
-            arr[j] = arr[sz-1]
+        arr += arrList[arr-1][j] + arrList[arr-1][j+1]
     arr += 1
     arrList += arr
+
+print(arrList)
