17c1be183b89ea823f76b57b852c5a44849e5565
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 19:39:25 +0700

    It doesn't work...

--- test01_20231017/exam02_3-coupleList.py
+++ test01_20231017/exam02_3-coupleList.py
@@ -12,7 +12,7 @@ for i in range(2, n+1):
     for j in range(0, sz+1):
         if j == 0:
             arrList[major][j] = arrList[minor][-1]
-        elif j < sz+1:
+        elif j < sz:
             arrList[major][j] = arrList[major][j-1] + arrList[minor][j-1]
         else:
             arrList[major] += arrList[major][j-1] + arrList[minor][j-1]
