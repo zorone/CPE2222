a3553d2a48901a987d0e76cc662ff6275b3beb1c
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 19:48:12 +0700

    Simplify coupleList method. (Count from backward.)

---
+++ test01_20231017/exam02_4-simplifyCoupleList.py
@@ -0,0 +1,24 @@
+n = int(input("Please Enter Degree: "))
+
+sz = 1
+arrList = [[1], [1, 2]]
+major = 1
+minor = 0
+
+for i in range(2, n+1):
+    major = minor
+    minor = (minor+1)%2
+    sz += 1
+    for j in range(0, sz+1):
+        if j == 0:
+            arrList[major][j] = arrList[minor][-1]
+        elif j < sz-1:
+            arrList[major][j] = arrList[major][-j] + arrList[minor][-j]
+        else:
+            arrList[major] += [arrList[major][-j] + arrList[minor][-j]]
+
+if(n < 2):
+    res = arrList[n]
+else:
+    res = arrList[major]
+print(res)
