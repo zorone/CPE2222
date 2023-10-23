3ceb96e3f420504ed0f8ae2a8c9d15df6c16f614
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 18:53:47 +0700

    Forgot that arrList won't add more element until loop is end.
    It still doesn't work correctly, though.

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -17,7 +17,7 @@ for i in range(1, n+1):
         if j == 0:
             val = arrList[sz-1][sz-1]
         else:
-            val = arrList[sz-1][j-1] + arrList[sz][j-1]
+            val = arrList[sz-1][j-1] + arr[j-1]
         arr += [val]
     arr += [1]
     arrList += [arr]
