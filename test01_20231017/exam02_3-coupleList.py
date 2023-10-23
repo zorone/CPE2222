fdbedacd646deddb3bb91ff768aa4e875a301557
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 19:27:11 +0700

    Somethings is wrong...

--- test01_20231017/exam02_3-coupleList.py
+++ test01_20231017/exam02_3-coupleList.py
@@ -8,15 +8,12 @@ minor = 0
 for i in range(1, n+1):
     major = minor
     minor = not minor
-    arrList[major] = list()
     sz += 1
     for j in range(0, sz+1):
         if j == 0:
-            val = arrList[minor][-1]
+            arrList[major][j] = arrList[minor][-1]
         else:
-            val = arrList[major][j-1] + arrList[minor][j-1]
-        arr += [val]
-    arrList += [arr]
+            arrList[major][j] = arrList[major][j-1] + arrList[minor][j-1]
 
 res = arrList[major]
 print(res)
