5dec228f553cf69cffd0a04ad195d19497e2023c
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 19:14:29 +0700

    Somethings is off...

--- test01_20231017/exam02_2-simplify.py
+++ test01_20231017/exam02_2-simplify.py
@@ -1,11 +1,12 @@
 n = int(input("Please Enter Degree: "))
 
+sz = 0
 arr = [1]
 arrList = list()
 arrList += [arr]
-sz = 1
 
 for i in range(1, n+1):
+    sz += 1
     arr = list()
     for j in range(0, sz+1):
         if j == 0:
@@ -14,7 +15,6 @@ for i in range(1, n+1):
             val = arrList[-1][-1] + arr[-1]
         arr += [val]
     arrList += [arr]
-    sz += 1
 
 res = arrList[sz]
 print(res)
