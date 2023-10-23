9bcc24d0b81e75a8ea6896dae1a155b0b2042284
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 18:57:58 +0700

    Remove remnant from last implementation that can mess up with result.

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -7,19 +7,13 @@ arrList += [arr]
 
 for i in range(1, n+1):
     arr = list()
-    arr += [1]
     sz = len(arrList)
-    for j in range(0, sz-1):
-        if sz < 2:
-            arr = [1, 2]
-            arrList += [arr]
-            continue
+    for j in range(0, sz):
         if j == 0:
             val = arrList[sz-1][sz-1]
         else:
             val = arrList[sz-1][j-1] + arr[j-1]
         arr += [val]
-    arr += [1]
     arrList += [arr]
 
 print(arrList)
