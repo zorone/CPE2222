4654d6b8e97b43938bb3f6993f29b408df33bf96
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 18:06:09 +0700

    still implement incorrectly.

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -6,11 +6,11 @@ arrList += arr
 
 for i in range(1, n+1):
     arr = list()
-    arr += 1
+    arr += [1]
     sz = len(arr)
     for j in range(0, n):
         arr += arrList[arr-1][j] + arrList[arr-1][j+1]
-    arr += 1
+    arr += [1]
     arrList += arr
 
 print(arrList)
