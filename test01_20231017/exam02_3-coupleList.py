44ae2692ec28d168520bc9ccc7c79fd11557596f
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 19:42:24 +0700

    Don't forgot that it need to create 2 more element when going from minor to major.

--- test01_20231017/exam02_3-coupleList.py
+++ test01_20231017/exam02_3-coupleList.py
@@ -12,7 +12,7 @@ for i in range(2, n+1):
     for j in range(0, sz+1):
         if j == 0:
             arrList[major][j] = arrList[minor][-1]
-        elif j < sz:
+        elif j < sz-1:
             arrList[major][j] = arrList[major][j-1] + arrList[minor][j-1]
         else:
             arrList[major] += arrList[major][j-1] + arrList[minor][j-1]
