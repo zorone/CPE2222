03f544689c75c00a3f78b6e6543c04bd37c1efe7
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 19:49:20 +0700

    It isn't quite work.

--- test01_20231017/exam02_4-simplifyCoupleList.py
+++ test01_20231017/exam02_4-simplifyCoupleList.py
@@ -13,9 +13,9 @@ for i in range(2, n+1):
         if j == 0:
             arrList[major][j] = arrList[minor][-1]
         elif j < sz-1:
-            arrList[major][j] = arrList[major][-j] + arrList[minor][-j]
+            arrList[major][j] = arrList[major][1-j] + arrList[minor][1-j]
         else:
-            arrList[major] += [arrList[major][-j] + arrList[minor][-j]]
+            arrList[major] += [arrList[major][1-j] + arrList[minor][1-j]]
 
 if(n < 2):
     res = arrList[n]
