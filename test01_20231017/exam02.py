6d2cf9e3784aca1d61c63ca4865ec6e9906c3ea7
Imron jehleh <imron.jehleh@gmail.com>
2023-10-23 17:59:44 +0700

    After test, this is trying to implement after testing.
    Try to implement exam02

--- test01_20231017/exam02.py
+++ test01_20231017/exam02.py
@@ -1,11 +1,15 @@
 n = int(input("Please Enter Degree: "))
 
 arr = [1]
+arrList = list()
+arrList += arr
 
-for i in range(0, n):
-    for j in range(0, n+1):
+for i in range(1, n):
+    arr = list()
+    arr += 1
+    for j in range(0, n):
         if(j == 0):
             sz = len(arr)
             arr[j] = arr[sz-1]
-        
-        
+    arr += 1
+    arrList += arr
