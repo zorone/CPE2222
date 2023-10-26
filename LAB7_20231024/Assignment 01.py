bbb3f2adcaa989a93c4e26fee2b34a688726fec0
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 17:58:55 +0700

    Still not right...

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -8,11 +8,13 @@ window.geometry("240x80")
 message = [Label(window, text="Guessing")]
 entry = list()
 
-frame = Frame(window)
+frame = Frame(window, padding=5)
+frame.grid(column=0, row=0, sticky=(N, W, E, S))
+
 frame.grid(row=0, column=0)
 
 for i in range(0, 4):
-    entry += [Entry(window, width=3)]
+    entry += [Entry(frame, width=3)]
     entry[i].grid(row=0, column=i+2)
 
 window.mainloop()
