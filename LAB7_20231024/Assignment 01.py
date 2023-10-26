61a121659c03198f2074b4a7dbbe3025accb847e
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 18:02:38 +0700

    Now that's something.

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -8,8 +8,7 @@ window.geometry("240x80")
 message = [Label(window, text="Guessing")]
 entry = list()
 
-frame = Frame(window)
-frame["padding"] = 5
+frame = Frame(window, borderwidth=5)
 frame.grid(column=0, row=0, sticky=(N, W, E, S))
 
 frame.grid(row=0, column=0)
