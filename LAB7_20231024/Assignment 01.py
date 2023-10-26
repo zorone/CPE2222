f257dfe5b8278a41709f2aa8a5912b03604eba13
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 14:10:13 +0700

    edit window size in correct format.

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -3,7 +3,7 @@ from random import sample
 
 window = Tk()
 window.title("Bull and Cow guessing game")
-window.geometry("480*80")
+window.geometry("480x80")
 
 message = [Label(window, text="Guessing")]
 entry = [Entry(window, width=3)]
