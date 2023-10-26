06fc1a042141f29fdb2adc4218492c7a79c8a310
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 19:01:52 +0700

    Dafaq?

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -5,15 +5,17 @@ ans = ''.join(sample('1234567890', 4))
 
 def check():
     hintStr = hintGenerate()
+    resFrame = Frame(frame, height=4)
+    resFrame.grid()
     if(hintStr[0] != 4):
         res = Label(frame, text="Hint:", justify='center')
-        res.grid(row=0, column=6)
+        res.grid(row=1, column=6)
         hint = Label(frame, text=hintStr[1], justify='center')
-        hint.grid(row=1, column=6)
+        hint.grid(row=2, column=6)
         return
     
     res = Label(frame, text="*** CORRECT ***", justify='center')
-    res.grid(row=0, column=6)
+    res.grid(row=1, column=6)
             
 def hintGenerate():
     bull = 0
@@ -34,23 +36,24 @@ window.geometry("320x120")
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 
-frame = Frame(window)
+frame = Frame(window, border=3)
 frame.anchor('center')
 frame.grid()
 
-message = Label(frame, text="Guessing:")
+displayFrame = Frame(frame, height=4)
+message = Label(displayFrame, text="Guessing:")
 message.grid(row=0, column=0)
 entry = list()
 
 for i in range(0, 4):
-    entry += [Entry(frame, width=3)]
+    entry += [Entry(displayFrame, width=3)]
     entry[i].grid(row=0, column=i+1)
 
 
-buttonFrame = Frame(frame, width=10)
+buttonFrame = Frame(frame, width=10, height=4)
 buttonFrame.anchor('center')
-buttonFrame.grid(row=0, column=5, columnspan=3)
+buttonFrame.grid()
 button = Button(buttonFrame, text="Submit", command=check)
-button.grid(row=0, column=1)
+button.grid(row=1, column=0)
 
 window.mainloop()
