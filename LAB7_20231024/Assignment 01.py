e706ee889ccfe9671a763466e515f43b3ab8440f
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 21:13:48 +0700

    Oh, shoot. Wrong variable.

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -4,17 +4,19 @@ from random import sample
 ans = ''.join(sample('1234567890', 4))
 
 def check():
+    resFrame.grid()
     hintStr = hintGenerate()
     if(hintStr[0] != 4):
-        print(ans)
-        res = Label(frame, text="Hint:", justify='center')
-        res.grid(row=0, column=6)
-        hint = Label(frame, text=hintStr[1], justify='center')
-        hint.grid(row=1, column=6)
+        print(res)
+        res = Label(resFrame, text="Hint:", justify='center')
+        res.grid(row=1, column=6)
+        hint = Label(resFrame, text=hintStr[1], justify='center')
+        hint.grid(row=2, column=6)
         return
     
-    res = Label(frame, text="*** CORRECT ***", justify='center')
-    res.grid(row=0, column=6)
+    hintStr = ''
+    res = Label(resFrame, text="*** CORRECT ***", justify='center')
+    res.grid(row=1, column=6)
             
 def hintGenerate():
     bull = 0
@@ -35,23 +37,29 @@ window.geometry("320x120")
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 
-frame = Frame(window)
+frame = Frame(window, border=3)
 frame.anchor('center')
-frame.grid()
+frame.grid(sticky=(N, W, E, S))
 
-message = Label(frame, text="Guessing:")
-message.grid(row=0, column=0)
+displayFrame = Frame(frame, height=4)
+displayFrame.anchor('w')
+displayFrame.grid()
+message = Label(displayFrame, text="Guessing:")
+message.grid(row=1, column=0)
 entry = list()
 
 for i in range(0, 4):
-    entry += [Entry(frame, width=3)]
-    entry[i].grid(row=0, column=i+1)
+    entry += [Entry(displayFrame, width=3)]
+    entry[i].grid(row=1, column=i+1)
 
 
-buttonFrame = Frame(frame, width=10)
+buttonFrame = Frame(frame, width=10, height=4)
 buttonFrame.anchor('center')
-buttonFrame.grid(row=0, column=5, columnspan=3)
+buttonFrame.grid()
 button = Button(buttonFrame, text="Submit", command=check)
-button.grid(row=0, column=1)
+button.grid(row=1, column=0)
+
+resFrame = Frame(frame, height=4)
+resFrame.anchor('e')
 
 window.mainloop()
