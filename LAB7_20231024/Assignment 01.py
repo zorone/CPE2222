777e549f8b72ab4a5925eeb300220312735fc8cc
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 18:30:31 +0700

    Progress!

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -7,13 +7,13 @@ def check():
     hintStr = hintGenerate()
     if(hintStr[0] != 4):
         res = Label(window, text="Hint:", justify='center')
-        res.grid(row=0, grid=5)
+        res.grid(row=0, grid=6)
         hint = Label(window, text=hintStr[1], justify='center')
-        hint.grid(row=1, grid=5)
+        hint.grid(row=1, grid=6)
         return
     
     res = Label(window, text="*** CORRECT ***", justify='center')
-    res.grid(row=0, grid=5)
+    res.grid(row=0, grid=6)
             
 def hintGenerate():
     bull = 0
@@ -31,7 +31,7 @@ window = Tk()
 window.title("Bull and Cow guessing game")
 window.geometry("320x120")
 
-message = Label(window, text="Guessing")
+message = Label(window, text="Guessing:")
 message.grid(row=0, column=0)
 entry = list()
 
@@ -40,5 +40,6 @@ for i in range(0, 4):
     entry[i].grid(row=0, column=i+1)
 
 button = Button(window, text="Submit", command=check)
+button.grid(row=0, column=5, columnspan=2)
 
 window.mainloop()
