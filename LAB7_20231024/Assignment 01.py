ae1a457a8b40355bef6060ecfecf63c8a48b3d97
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 20:55:55 +0700

    Test, find where it was good before.

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -4,18 +4,19 @@ from random import sample
 ans = ''.join(sample('1234567890', 4))
 
 def check():
+    resFrame.grid()
     hintStr = hintGenerate()
-    hint = Label(frame, text=hintStr[1], padx=2, justify='center')
     if(hintStr[0] != 4):
+        resStr = "Hint:"
         print(ans)
-        res = Label(frame, text="Hint:", padx=2, justify='center')
-        res.grid(row=1, column=6)
-        hint.grid(row=2, column=6)
         
     else:
-        res = Label(frame, text="Hint:", padx=2, justify='center')
-        res.grid(row=1, column=6)
-        hint.destroy()
+        resStr = "*** CORRECT ***"
+        
+    res = Label(resFrame, text=resStr, justify='center')
+    res.grid(row=1, column=6)
+    hint = Label(resFrame, text=hintStr[1], justify='center')
+    hint.grid(row=2, column=6)
             
 def hintGenerate():
     bull = 0
@@ -32,7 +33,7 @@ def hintGenerate():
 
 window = Tk()
 window.title("Bull and Cow guessing game")
-window.geometry("320x100")
+window.geometry("320x120")
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 
@@ -40,15 +41,26 @@ frame = Frame(window, border=3)
 frame.anchor('center')
 frame.grid(sticky=(N, W, E, S))
 
-message = Label(frame, text="Guessing:")
+displayFrame = Frame(frame, height=4)
+displayFrame.anchor('w')
+displayFrame.pack(side='left')
+displayFrame.grid()
+message = Label(displayFrame, text="Guessing:")
 message.grid(row=1, column=0)
 entry = list()
 
 for i in range(0, 4):
-    entry += [Entry(frame, width=3)]
+    entry += [Entry(displayFrame, width=3)]
     entry[i].grid(row=1, column=i+1)
 
-button = Button(frame, text="Submit", padx=2, command=check)
-button.grid(row=1, column=5)
+
+buttonFrame = Frame(frame, width=10, height=4)
+buttonFrame.anchor('center')
+buttonFrame.grid()
+button = Button(buttonFrame, text="Submit", command=check)
+button.grid(row=1, column=0)
+
+resFrame = Frame(frame, height=4)
+resFrame.anchor('e')
 
 window.mainloop()
