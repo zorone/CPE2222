d737dd3f209469c26512c5f1d939cc071577886b
Imron jehleh <imron.jehleh@gmail.com>
2023-10-26 14:08:05 +0700

    Start Assignment 01

---
+++ LAB7_20231024/Assignment 01.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from random import sample
+
+window = Tk()
+window.title("Bull and Cow guessing game")
+window.geometry("480*80")
+
+message = [Label(window, text="Guessing")]
+entry = [Entry(window, width=3)]
+entry = entry*4
+
+window.mainloop()
