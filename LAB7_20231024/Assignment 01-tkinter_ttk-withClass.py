6958e878c52475b57cd1a7cc6dce84ba41f05647
Imron jehleh <imron.jehleh@gmail.com>
2023-10-28 18:37:00 +0700

    Ah, that's hard.

--- LAB7_20231024/Assignment 01-tkinter_ttk-withClass.py
+++ LAB7_20231024/Assignment 01-tkinter_ttk-withClass.py
@@ -2,10 +2,16 @@ from tkinter import *
 from tkinter import ttk
 from random import sample
 
-class game:
-    def __init__(self):
-        print("Hello python Class.")
+class core:
+    def __init__(self, root):
+        root.title("Bull and Cow guessing game")
+        root.geometry("360x80")
+        
+        
+        
+        ans = str()
 
 if __name__ == "__main__":
     window = Tk()
+    core(window)
     window.mainloop()
