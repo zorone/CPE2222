22c2b606ddf11fa9e124c9869cfba8e7a52af5ee
Imron jehleh <imron.jehleh@gmail.com>
2023-10-28 18:15:47 +0700

    Try challenge myself with class.

---
+++ LAB7_20231024/Assignment 01-tkinter_ttk-withClass.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+from random import sample
+
+class game:
+    def __init__(self):
+        print("Hello python Class.")
+
+if __name__ == "__main__":
+    window = Tk()
+    window.mainloop()
