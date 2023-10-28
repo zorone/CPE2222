2f6ad5b7bd5c3d66e729aa0ee571e8a096d0f4f5
Imron jehleh <imron.jehleh@gmail.com>
2023-10-28 18:41:48 +0700

    My head hurts.

--- LAB7_20231024/Assignment 01-tkinter_ttk-withClass.py
+++ LAB7_20231024/Assignment 01-tkinter_ttk-withClass.py
@@ -1,15 +1,10 @@
-from tkinter import *
+from tkinter import tk
 from tkinter import ttk
 from random import sample
 
-class core:
-    def __init__(self, root):
-        root.title("Bull and Cow guessing game")
-        root.geometry("360x80")
+class Main(tk.Tk):
+    __init__(self):
         
-        
-        
-        ans = str()
 
 if __name__ == "__main__":
     window = Tk()
