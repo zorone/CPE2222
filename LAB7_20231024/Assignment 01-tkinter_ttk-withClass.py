cebc7fb3eda65f81fe96cd13b1eb702c42c8be41
Imron jehleh <imron.jehleh@gmail.com>
2023-10-28 18:48:51 +0700

    Huff, that's really hard.

--- LAB7_20231024/Assignment 01-tkinter_ttk-withClass.py
+++ LAB7_20231024/Assignment 01-tkinter_ttk-withClass.py
@@ -1,12 +1,18 @@
+from tkinter import *
 from tkinter import tk
 from tkinter import ttk
 from random import sample
 
 class Main(tk.Tk):
-    __init__(self):
+    def __init__(self):
+        self.title("Bull and Cow guessing game")
+        self.geometry("360x80")
         
+        self.mainFrame = ttk.Frame(self, padding=5)
+        self.mainFrame.grid(column=0, row=0, sticky=(N,W,E,S))
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
 if __name__ == "__main__":
-    window = Tk()
-    core(window)
-    window.mainloop()
+    main = Main()
+    main.mainloop()
