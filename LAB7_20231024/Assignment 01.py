a5d1606341f6432f690fde140734f68ac893af73
Imron jehleh <imron.jehleh@gmail.com>
2023-10-28 17:01:50 +0700

    Oopsie.

--- LAB7_20231024/Assignment 01.py
+++ LAB7_20231024/Assignment 01.py
@@ -8,15 +8,15 @@ def check():
     if(hintStr[0] != 4):
         print(ans)
         res = Label(window, text="Hint:", padx=2, justify='center')
-        res.grid(row=1, column=6)
+        res.grid(row=1, column=9)
 
     else:
         res = Label(window, text="*** CORRECT ***", padx=2, justify='center')
-        res.grid(row=1, column=6)
+        res.grid(row=1, column=9)
         hintStr[1] = ""
 
     hint = Label(window, text=hintStr[1], padx=2, justify='center')
-    hint.grid(row=2, column=6)
+    hint.grid(row=2, column=9)
     
 def hintGenerate():
     bull = 0
@@ -44,14 +44,16 @@ message = Label(window, text="Guessing:")
 message.grid(row=1, column=0)
 entry = list()
 
-buttonPadding.grid(row=1, column=1)
-
 for i in range(0, 4):
     entry += [Entry(window, width=3)]
-    entry[i].grid(row=1, column=i+2)
+    entry[i].grid(row=1, column=i+1)
+
+buttonPadding.grid(row=1, column=6)
 
 button = Button(window, text="Submit", padx=2, command=check)
 button.grid(row=1, column=7)
+
+buttonPadding.grid(row=1, column=8)
 
 padding.grid(row=3, column=0)
 
