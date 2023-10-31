dff4243ca140b813dd9865c4170e2408ebc1c5b7
Imron jehleh <imron.jehleh@gmail.com>
2023-10-31 14:48:32 +0700

    Change some algorithm.

--- LAB8_20231031/Assignment 02.py
+++ LAB8_20231031/Assignment 02.py
@@ -17,16 +17,20 @@ class Main(Tk):
         self.calcEntry = ["Area of Rectangle", "Area of Triangle", "Pythagorean"]
         self.calcSelected = IntVar()
         
+        self.pane = False
+        
         for i, title in enumerate(self.calcEntry):
             self.calcEntry[i] = ttk.Radiobutton(self.calcOption, text=title, value=i, variable=self.calcSelected, command=self.calcOptionSet)
             self.calcEntry[i].pack(side='top', anchor='w')
         
     def calcOptionSet(self):
-        if self.calcSelected == 0:
+        self.paramGenerate()
             
 
     def paramGenerate(self):
-        print()
+        if not self.pane:
+            self.pane = True
+        
 
 if __name__ == "__main__":
     main = Main()
