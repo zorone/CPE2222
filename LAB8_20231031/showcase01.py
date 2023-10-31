ecc316c8f0059efc79a08e9b64ea20b7659595d2
Imron jehleh <imron.jehleh@gmail.com>
2023-10-31 20:53:35 +0700

    Implement recursive codes

--- LAB8_20231031/showcase01.py
+++ LAB8_20231031/showcase01.py
@@ -2,6 +2,6 @@ def recursive(*arg):
     sz = len(arg)
     if(sz > 1):
         recursive(arg[0:sz-1])
-        print
-    else:
-        print(arg)
+    print(arg)
+    
+recursive("Hello World!")
