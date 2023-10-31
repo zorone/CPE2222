a6140b25bb7adde6cda3a122bd308ecbcaa635d3
Imron jehleh <imron.jehleh@gmail.com>
2023-10-31 21:00:15 +0700

    Successfully implement factorial.

--- LAB8_20231031/showcase01.py
+++ LAB8_20231031/showcase01.py
@@ -1,7 +1,10 @@
 def factorial(num):
     if(0 <= num <= 1):
         print(num)
+        return num
     else:
-        print(num*factorial(num-1))
+        num *= factorial(num-1)
+        print(num)
+        return num
         
 factorial(10)
