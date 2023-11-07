3f0aad03401257cfe96882079674e4b355fca77e
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 21:00:30 +0700

    Look better.

--- LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
+++ LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
@@ -14,12 +14,13 @@ class Main(Tk):
         self.optionText = ['Area of Rectangle', 'Area of Triangle', 'Pythagorean']
         
         self.expand = False
-        self.command = self.trigger()
         
         self.mainFrame = self.buildFrame(self, x=0, y=0, width=3, height=7)
         self.optionFrame = self.buildLabelFrame(self.mainFrame, text="Calculation", x=0, y=0, width=1, height=3)
         self.optionList = self.buildRadioButton(self.optionFrame, self.option, self.optionText, amount=3)
-        self.setCommand(self.optionList, self.command)
+        
+        for option in self.optionList:
+            option.config(command=self.trigger)
         
     def trigger(self):
         if not self.expand:
@@ -35,13 +36,13 @@ class Main(Tk):
         self.paramLabel = self.buildLabel(self.actionFrame, self.paramText, 3, pos=(E))
 
     def buildFrame(self, host, x, y, width=1, height=1, pos=(N,W,E,S)):
-        res = ttk.Labelframe(host, padding=5)
+        res = ttk.Frame(host, padding=5)
         res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
         return res
 
     def buildLabelFrame(self, host, text, x, y, width=1, height=1, pos=(N,W,E,S)):
         res = ttk.Labelframe(host, text=text, padding=5)
-        res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
+        res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos, padx=5)
         return res
     
     def buildLabel(self, host, textList, amount=1, pos=(N,W,E,S)):
@@ -56,10 +57,6 @@ class Main(Tk):
             res += [ttk.Radiobutton(host, text=textList[i], value=i, variable=var)]
             res[i].grid(column=0, row=i, sticky=pos)
         return res
-
-    def setCommand(self, varList, command):
-        for i, item in enumerate(varList):
-            item.config(command=command)
 
 if __name__ == "__main__":
     main = Main()
