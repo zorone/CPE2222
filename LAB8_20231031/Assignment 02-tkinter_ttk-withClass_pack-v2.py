11a026b1369c2b34aaf03c047ed788a101052144
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 19:28:39 +0700

    ...

--- LAB8_20231031/Assignment 02-tkinter_ttk-withClass_pack-v2.py
+++ LAB8_20231031/Assignment 02-tkinter_ttk-withClass_pack-v2.py
@@ -1,16 +1,22 @@
 from tkinter import *
 from tkinter import ttk
 
+class Build:
+    def __init__(self,):
+        print()
+    def frame(self, x, y, width, height):
+        res = list()
+        res += 
 class Main(Tk):
+    
+    build = Build()
+    
     def __init__(self,):
         super().__init__()
         
         self.title("CPE2222")
         self.style = ttk.Style()
-    
-class Build:
-    def __init__(self,):
-        print()
+        
 
 if __name__ == "__main__":
     main = Main()
