fccb1460860778c85a74bceeda4000fce1bcef19
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 20:10:40 +0700

    More implementation.

--- LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
+++ LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
@@ -10,8 +10,12 @@ class Main(Tk):
         self.title("CPE2222")
         self.style = ttk.Style()
         
+        self.option = IntVar()
+        self.optionText = ('Area of Rectangle', 'Area of Triangle', 'Pythagorean')
+        
         self.mainFrame = self.buildFrame(self, x=0, y=0, width=3, height=7)
         self.optionFrame = self.buildLabelFrame(self.mainFrame, text="Calculation", x=0, y=0, width=1, height=3)
+        self.optionList = self.buildRadioButton(self.optionFrame, self.option, 3,)
 
     def buildFrame(self, host, x, y, width=1, height=1, pos=(N,W,E,S)):
         res = ttk.Labelframe(host, padding=5)
@@ -23,10 +27,12 @@ class Main(Tk):
         res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
         return res
     
-    def buildRadioButton(self, host, amount=1, textList=list(), pos=(N,W,E,S)):
+    def buildRadioButton(self, host, var, amount=1, textList=list(), pos=(N,W,E,S)):
         res = list()
         for i in range(amount):
-            res += [ttk.Radiobutton(host, text=textList[i], value=i)]
+            res += [ttk.Radiobutton(host, text=textList[i], value=i, variable=var)]
+            res[i].grid(column=0, row=i, sticky=pos)
+        return res
 
 if __name__ == "__main__":
     main = Main()
