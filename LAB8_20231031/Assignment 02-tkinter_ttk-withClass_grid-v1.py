729f1361b7e82e1a8781777863861577aea2df1c
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 20:06:10 +0700

    Implement more build function.

--- LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
+++ LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
@@ -10,11 +10,23 @@ class Main(Tk):
         self.title("CPE2222")
         self.style = ttk.Style()
         
-        self.mainFrame = ttk.Frame(self, padding=5)
-        self.mainFrame.grid(row=0, column=0, rowspan=3, columnspan=3, sticky=(N,W,E,S))
-        
-        self.optionFrame = ttk.Labelframe(self.mainFrame, text="Calculation", padding=5)
-        self.optionFrame.grid(row=0, column=0, sticky=(N,W,E,S))
+        self.mainFrame = self.buildFrame(self, x=0, y=0, width=3, height=7)
+        self.optionFrame = self.buildLabelFrame(self.mainFrame, text="Calculation", x=0, y=0, width=1, height=3)
+
+    def buildFrame(self, host, x, y, width=1, height=1, pos=(N,W,E,S)):
+        res = ttk.Labelframe(host, padding=5)
+        res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
+        return res
+
+    def buildLabelFrame(self, host, text, x, y, width=1, height=1, pos=(N,W,E,S)):
+        res = ttk.Labelframe(host, text=text, padding=5)
+        res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
+        return res
+    
+    def buildRadioButton(self, host, amount=1, textList=list(), pos=(N,W,E,S)):
+        res = list()
+        for i in range(amount):
+            res += [ttk.Radiobutton(host, text=textList[i], value=i)]
 
 if __name__ == "__main__":
     main = Main()
