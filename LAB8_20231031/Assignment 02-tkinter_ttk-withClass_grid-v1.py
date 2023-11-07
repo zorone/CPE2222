4e7f921c8d315631534fd3cd5215ccd507fa3c2b
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 19:54:08 +0700

    ...

--- LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
+++ LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
@@ -11,18 +11,16 @@ class Main(Tk):
         self.style = ttk.Style()
         
         self.mainFrame = self.buildFrame(self, x=0, y=0, width=3, height=7)
+        self.optionFrame = self.optionFrame(self.mainFrame, x=0, y=0, width=1, height=1)
 
-    def buildFrame(self, host, x, y, width, height, pos):
+    def buildFrame(self, host, x, y, width, height, pos=(N,W,E,S)):
         res = ttk.Labelframe(host, padding=5)
-        if pos != (N,W,E,S):
-            res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
-        else:
-            res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=(N,W,E,S))
+        res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
         return res
 
-    def buildLabelFrame(self, host, text, x, y, width, height):
+    def buildLabelFrame(self, host, text, x, y, width, height, pos=(N,W,E,S)):
         res = ttk.Labelframe(host, text=text, padding=5)
-        res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=(N,W,E,S))
+        res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
         return res
 
 if __name__ == "__main__":
