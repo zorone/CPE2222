7d76d5b06c21583dbd557d0155a8636c79f48e57
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 19:56:36 +0700

    It won't work.

--- LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
+++ LAB8_20231031/Assignment 02-tkinter_ttk-withClass_grid-v1.py
@@ -11,14 +11,14 @@ class Main(Tk):
         self.style = ttk.Style()
         
         self.mainFrame = self.buildFrame(self, x=0, y=0, width=3, height=7)
-        self.optionFrame = self.optionFrame(self.mainFrame, x=0, y=0, width=1, height=1)
+        self.optionFrame = self.buildLabelFrame(self.mainFrame, text="Calculation", x=0, y=0, width=1, height=3)
 
-    def buildFrame(self, host, x, y, width, height, pos=(N,W,E,S)):
+    def buildFrame(self, host, x, y, width=1, height=1, pos=(N,W,E,S)):
         res = ttk.Labelframe(host, padding=5)
         res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
         return res
 
-    def buildLabelFrame(self, host, text, x, y, width, height, pos=(N,W,E,S)):
+    def buildLabelFrame(self, host, text, x, y, width=1, height=1, pos=(N,W,E,S)):
         res = ttk.Labelframe(host, text=text, padding=5)
         res.grid(column=x, row=y, columnspan=width, rowspan=height, sticky=pos)
         return res
