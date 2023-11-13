8f227ae050e35032213957508f2d681d47cf4e50
Imron jehleh <imron.jehleh@gmail.com>
2023-11-13 15:15:42 +0700

    More implementation.

--- LAB9_20231107/3-canvas_mixed.py
+++ LAB9_20231107/3-canvas_mixed.py
@@ -8,17 +8,21 @@ class Main(Tk):
         self.geometry('270x165')
         self.title('Canvas Drawing')
         
+        self.rectangleSet = 0
+        self.triangleSet = 0
+        self.circleSet = 0
+        
         self.mainFrame = ttk.Frame(self, padding=5)
         self.mainFrame.pack(anchor='center', side='top', fill='both', expand=TRUE)
         
-        self.optionFrame = ttk.Labelframe(self.mainFrame, padding=5, text='Drawing Setting')
-        self.optionFrame.pack(anchor='center', side='top', fill='both', expand=TRUE)
+        self.optionFrame = ttk.Labelframe(self.mainFrame, text='Drawing Setting')
+        self.optionFrame.pack(anchor='center', side='top', fill='both', expand=TRUE, ipad=5, ipady=5, padx=5, pady=5)
         
-        self.drawButton = ttk.button(self.mainFrame, padding=5, )
+        self.drawButton = ttk.Button(self.mainFrame, padding=5, text='Draw')
+        self.drawButton.pack(anchor='center', side='top', fill='both', expand=TRUE)
+        
+        self.rectangleTick = ttk.Checkbutton(self.optionFrame, name='Rectangle', onvalue=1, offvalue=0, variable=self.rectangleSet)
         
 if __name__ == '__main__':
     main = Main()
     main.mainloop()
-    
-size = [[50, 50], [50, 100], 75, ]
-if(size[0][0])
