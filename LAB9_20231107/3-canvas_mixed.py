cf9ef0096316354d2d1eba6d36d575fdad3e2df6
Imron jehleh <imron.jehleh@gmail.com>
2023-11-13 15:08:11 +0700

    Try to implement calculation for canvas size.

--- LAB9_20231107/3-canvas_mixed.py
+++ LAB9_20231107/3-canvas_mixed.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+import math
 
 class Main(Tk):
     def __init__(self):
@@ -8,11 +9,16 @@ class Main(Tk):
         self.title('Canvas Drawing')
         
         self.mainFrame = ttk.Frame(self, padding=5)
-        self.mainFrame.pack(anchor='center', side='top')
+        self.mainFrame.pack(anchor='center', side='top', fill='both', expand=TRUE)
         
         self.optionFrame = ttk.Labelframe(self.mainFrame, padding=5, text='Drawing Setting')
-        self.optionFrame.pack()
+        self.optionFrame.pack(anchor='center', side='top', fill='both', expand=TRUE)
+        
+        self.drawButton = ttk.button(self.mainFrame, padding=5, )
         
 if __name__ == '__main__':
     main = Main()
     main.mainloop()
+    
+size = [[50, 50], [50, 100], 75, ]
+if(size[0][0])
