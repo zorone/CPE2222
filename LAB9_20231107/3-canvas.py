aef43530ece7a7e591851c637bc17d35a1466edd
Imron jehleh <imron.jehleh@gmail.com>
2023-11-13 10:45:14 +0700

    ?

--- LAB9_20231107/3-canvas.py
+++ LAB9_20231107/3-canvas.py
@@ -7,17 +7,17 @@ class Main(Tk):
         self.title('Canvas Drawing')
         
         self.mainFrameOption = {
-            x: 0,
-            y: 0,
+            'x': 0,
+            'y': 0,
         }
-        self.mainFrame = buildFrame(self, self.mainFrameOption)
+        self.mainFrame = self.buildFrame(self, self.mainFrameOption)
         
     def buildFrame(self, root, **options):
         res = ttk.Frame(root, padding=5)
         res.grid()
         return res
         
-    def buildLabelFrame(self, root, text, **options):
+    def buildLabelFrame(self, root, text, **options, **positions):
         res = ttk.LabelFrame(root, padding=5, text=text)
         res.grid()
         return res
