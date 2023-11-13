3cd19284aa07978cf32a64c94ae86d303ac87c87
Imron jehleh <imron.jehleh@gmail.com>
2023-11-13 14:19:44 +0700

    More implementation.

--- LAB9_20231107/3-canvas_mixed.py
+++ LAB9_20231107/3-canvas_mixed.py
@@ -3,6 +3,16 @@ from tkinter import ttk
 from tkinter import messagebox
 
 class Main(Tk):
-    __init__(self):
-        print()
+    def __init__(self):
+        self.geometry('270x165')
+        self.title('Canvas Drawing')
         
+        self.mainFrame = ttk.Frame(self, padding=5)
+        self.mainFrame.pack(anchor='center', side='top')
+        
+        self.optionFrame = ttk.Labelframe(self.mainFrame, padding=5, text='Drawing Setting')
+        self.optionFrame.pack()
+        
+if __name__ == '__main__':
+    main = Main()
+    main.mainloop()
