80e1f8b9f17e088dc705aafc4bb266cb63c29e41
Imron jehleh <imron.jehleh@gmail.com>
2023-11-13 16:22:23 +0700

    ...

--- LAB9_20231107/3-canvas_mixed.py
+++ LAB9_20231107/3-canvas_mixed.py
@@ -37,21 +37,21 @@ class Main(Tk):
         self.optionList += [ttk.OptionMenu(self.optionFrame, self.triangleVal, *self.polygonOption)]
         self.optionList += [ttk.OptionMenu(self.optionFrame, self.circleVal, *self.radiusOption)]
         
-        self.rectangleTick = ttk.Checkbutton(self.optionFrame, text='Rectangle', onvalue=1, offvalue=0, variable=self.optionSet[0], command=self.showOption(0))
+        self.rectangleTick = ttk.Checkbutton(self.optionFrame, text='Rectangle', onvalue=1, offvalue=0, variable=self.optionSet[0], command=self.showOption())
         self.rectangleTick.anchor('w')
         self.rectangleTick.grid(column=0, row=0, sticky=(N,W,E,S))
         
-        self.triangleTick = ttk.Checkbutton(self.optionFrame, text='Right Triangle', onvalue=1, offvalue=0, variable=self.optionSet[1], command=self.showOption(1))
+        self.triangleTick = ttk.Checkbutton(self.optionFrame, text='Right Triangle', onvalue=2, offvalue=0, variable=self.optionSet[1], command=self.showOption())
         self.triangleTick.anchor('w')
         self.triangleTick.grid(column=0, row=1, sticky=(N,W,E,S))
         
-        self.circleTick = ttk.Checkbutton(self.optionFrame, text='Circle', onvalue=1, offvalue=0, variable=self.optionSet[2], command=self.showOption(2))
+        self.circleTick = ttk.Checkbutton(self.optionFrame, text='Circle', onvalue=3, offvalue=0, variable=self.optionSet[2], command=self.showOption())
         self.circleTick.anchor('w')
         self.circleTick.grid(column=0, row=2, sticky=(N,W,E,S))
         
     def showOption(self, type):
         print(type)
-        if(self.optionSet[type].get() == 1):
+        if((self.optionSet[type].get()) - 1 == 1):
             self.optionLabel[type].grid(column=1, row=type, sticky=(N,W,E,S))
             self.optionList[type].grid(column=2, row=type, sticky=(N,W,E,S))
         else:
