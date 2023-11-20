dbba3c4ba21bc8d4eeac33b5d127631486906d36
Imron jehleh <imron.jehleh@gmail.com>
2023-11-20 23:25:52 +0700

    I think that should be good enough.

--- LAB10_20231114/Assignment 4.py
+++ LAB10_20231114/Assignment 4.py
@@ -19,10 +19,48 @@ class Main():
             
             F = open("input.txt", 'r', encoding='utf-8')
             data = F.readlines()
-            tempName = s[0]
+            tempLock = False
+            tempSkip = False
+            nameType = list()
+            tempName = ''
+            buffer = ''
             
             for s in data:
+                if not tempLock:
+                    tempName = s
+                    tempLock = True
+                    continue
                 
+                if not tempSkip:
+                    if len(nameType) > 1:
+                        nameType.clear()
+                        tempSkip = True
+                        continue
+                    if "นักศึกษา" in s:
+                        student += [s]
+                        tempSkip = True
+                        continue
+                    if "อาจารย์" in s:
+                        instructor += [s]
+                        nameType += ["instructor"]
+                        continue
+                    if "โยธา" in s:
+                        CVE += [s]
+                        nameType += ["CVE"]
+                        continue
+                    if "อุตสาหการ" in s:
+                        IND += [s]
+                        nameType += ["IND"]
+                        continue
+                    if "คอมพิวเตอร์" in s:
+                        CPE += [s]
+                        nameType += ["CPE"]
+                        continue
+                    
+                if s == '\n':
+                    tempLock = False
+                    tempSkip = False
+
         except OSError:
             print("Incorrect file Structure, please check if your workspace has all files that is needed.")
 
