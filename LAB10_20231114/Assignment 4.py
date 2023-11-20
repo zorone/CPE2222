67115e9561672f569e0c53d009699fb5aab16395
Imron jehleh <imron.jehleh@gmail.com>
2023-11-20 22:41:35 +0700

    ...

--- LAB10_20231114/Assignment 4.py
+++ LAB10_20231114/Assignment 4.py
@@ -4,7 +4,16 @@ class Main():
     def __init__(self):
         try:
             path = os.path.dirname(os.path.abspath(__file__))
-            print(path)
+            path = os.listdir(path)
+            if "readme.txt" not in path:
+                raise OSError
+            if "input.txt" not in path:
+                raise OSError
+            
+            structure = {"นักศึกษา": "นักศึกษา", "อาจารย์": ["คณาจารย์", "โยธา", "อุตสาหการ", "คอมพิวเตอร์"]}
+            
+            for route in structure:
+                
                 
         except OSError:
             print("Incorrect file Structure, please check if your workspace has all files that is needed.")
