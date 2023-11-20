5e24fbe8ac37a7594f24abe668be56a3044ac7ca
Imron jehleh <imron.jehleh@gmail.com>
2023-11-20 22:53:37 +0700

    Try to implement routing.

--- LAB10_20231114/Assignment 4.py
+++ LAB10_20231114/Assignment 4.py
@@ -4,19 +4,27 @@ class Main():
     def __init__(self):
         try:
             path = os.path.dirname(os.path.abspath(__file__))
-            path = os.listdir(path)
+            os.chdir(path)
+            path = os.listdir()
             if "readme.txt" not in path:
                 raise OSError
             if "input.txt" not in path:
                 raise OSError
             
-            structure = {"นักศึกษา": "นักศึกษา", "อาจารย์": ["คณาจารย์", "โยธา", "อุตสาหการ", "คอมพิวเตอร์"]}
+            structure = {"นักศึกษา": {"นักศึกษา": "นักศึกษา"},
+                         "อาจารย์": {"อาจารย์": "คณาจารย์", "โยธา": "โยธา", "อุตสาหการ": "อุตสาหการ", "คอมพิวเตอร์": "คอมพิวเตอร์"}}
             
-            for route in structure:
-                
+            route(structure)
                 
         except OSError:
             print("Incorrect file Structure, please check if your workspace has all files that is needed.")
+
+def route(r):
+    key = r.keys()
+    
+    for k in key:
+        if type(r[k]) == type(dict()):
+            route(r[k])
 
 if __name__ == '__main__':
     main = Main()
