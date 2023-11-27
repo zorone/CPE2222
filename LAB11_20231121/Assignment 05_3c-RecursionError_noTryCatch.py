54fb3bd1fb10a184687373f50367d342cc01bdb0
Imron jehleh <imron.jehleh@gmail.com>
2023-11-28 01:00:26 +0700

    Still to high...

--- LAB11_20231121/Assignment 05_3c-RecursionError_noTryCatch.py
+++ LAB11_20231121/Assignment 05_3c-RecursionError_noTryCatch.py
@@ -12,6 +12,6 @@ limit = getrecursionlimit()
 if(val >= limit):
     print('Escaping RecursionError')
     print("Set val to {} so it won't exceed recursion limit.".format(limit))
-    val = limit-2
+    val = limit-3
 
 recursivePrint(val)
