c904e1f6e2360513341da84bad47a5231051b502
Imron jehleh <imron.jehleh@gmail.com>
2023-11-28 01:23:28 +0700

    Now that's worked.

--- LAB11_20231121/Assignment 05_4a-IndentationError.py
+++ LAB11_20231121/Assignment 05_4a-IndentationError.py
@@ -1,2 +1,3 @@
-for i in range(1, 101):
-print(i)
+cmd = "for i in range(1, 101):\nprint(i)"
+codeObj = compile(cmd, 'test', 'exec')
+exec(codeObj)
--- LAB11_20231121/Assignment 05_4b-IndentationError.py
+++ LAB11_20231121/Assignment 05_4b-IndentationError.py
@@ -1,5 +1,6 @@
 try:
-    for i in range(1, 101):
-   print(i)
+    cmd = "for i in range(1, 101):\nprint(i)"
+    codeObj = compile(cmd, 'test', 'exec')
+    exec(codeObj)
 except IndentationError:
     print('Escaping IndentationError')
