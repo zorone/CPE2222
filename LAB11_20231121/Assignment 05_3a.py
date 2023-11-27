3d858bc322d33ccb95dafd239ecb831d2cf8f2c2
Imron jehleh <imron.jehleh@gmail.com>
2023-11-28 00:47:44 +0700

    Try implement new exception.

---
+++ LAB11_20231121/Assignment 05_3a.py
@@ -0,0 +1,9 @@
+def recursivePrint(val :int):
+    if(val > 0):
+        recursivePrint(val-1)
+    elif(val < 0):
+        recursivePrint(val+1)
+    print(val)
+
+input = int('Enter value:')
+recursivePrint(input)
