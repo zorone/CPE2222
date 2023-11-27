1b807362c201886238f89df0e61946b8ba22c2bd
Imron jehleh <imron.jehleh@gmail.com>
2023-11-28 00:09:31 +0700

    ...

---
+++ LAB11_20231121/Assignment 05_2c-TypeError_noTryCatch.py
@@ -0,0 +1,18 @@
+x = input("Enter first argument:")
+y = input("Enter second argument:")
+print("Usable modes: +, -, *, /")
+mode = input("Please enter modes:")
+
+if(mode == '+'):
+    res = x + y
+elif(y.isdecimal()):
+    if(mode == '*'):
+        res = x*y
+elif(x.isdigit):
+elif(mode == '-'):
+    res = x - y
+elif(mode == '/'):
+    res = x/y
+else:
+    print('Operation is unavailable.')
+print(res)
