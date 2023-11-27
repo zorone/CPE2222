3fa6f704085a0dbf9772ba2fda87c1390c4723fa
Imron jehleh <imron.jehleh@gmail.com>
2023-11-27 23:38:24 +0700

    Added no try-Catch Implement.

---
+++ LAB11_20231121/Assignment 05_1c-nameError_noTryCatch.py
@@ -0,0 +1,10 @@
+dup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+res = list()
+# only show list of itself when data no less than 5; else show accumulate list until this iterations.
+
+for data in dup:
+    if(data >= 5):
+        res = [data]
+    else:
+        res += [data]
+    print(res)
