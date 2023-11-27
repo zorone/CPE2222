26c3dc376900578708ebf6c9930ce80477e304de
Imron jehleh <imron.jehleh@gmail.com>
2023-11-27 23:33:08 +0700

    Change some codes structure.

--- LAB11_20231121/Assignment 05_1b-nameError.py
+++ LAB11_20231121/Assignment 05_1b-nameError.py
@@ -1,12 +1,14 @@
 dup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
+# only show list of itself when data no less than 5; else show accumulate list until this iterations.
+
 try:
     for data in dup:
         if(data >= 5):
             res = [data]
         else:
             res += [data]
+    print(res)
+
 except NameError:
     res = list()
-
-print(res)
