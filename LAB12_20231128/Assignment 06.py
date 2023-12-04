97f0b93dbf54ff390e45825fd474d0c97b4f1c8f
Imron jehleh <imron.jehleh@gmail.com>
2023-12-04 15:20:04 +0700

    ...

--- LAB12_20231128/Assignment 06.py
+++ LAB12_20231128/Assignment 06.py
@@ -44,15 +44,13 @@ print(J)
 J = np.sum((A*X-Y)**2)
 print(J)
 
-J = np.sum((A*X-Y)**2)/(2*500)
+J = np.sum((A*X-Y)**2)/(500)
 print(J)
 
-J = np.sum((A*X-Y)/(2*500))**2
+J = np.sum((A*X-Y)/(500))**2
 print(J)
 
-J = (np.sum(A*X-Y)**2)/(2*500)
+J = (np.sum(A*X-Y)**2)/(500)
 print(J)
 
-J = (np.sum(A*X-Y)/(2*500))**2
-print(J)
 # print(K)
