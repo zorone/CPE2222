a5cc7ddc4daced998a5cc6085ef9de71f5a93b43
Imron jehleh <imron.jehleh@gmail.com>
2023-12-04 11:20:21 +0700

    Initialize Assignment 6

---
+++ LAB12_20231128/Assignment 06.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+_1 = np.loadtxt('1.csv')
+_2 = np.loadtxt('2.csv')
+_3 = np.loadtxt('3.csv')
+_4 = np.loadtxt('4.csv')
+_5 = np.loadtxt('5.csv')
+
+print(_1)
+print(_2)
+print(_3)
+print(_4)
+print(_5)
