e727cba36b8d72b22f38f9ebc2c688b49504637b
Imron jehleh <imron.jehleh@gmail.com>
2023-12-04 16:10:07 +0700

    ...

--- LAB12_20231128/Assignment 06.py
+++ LAB12_20231128/Assignment 06.py
@@ -26,10 +26,10 @@ print('Y[{},0] = {}'.format(0, Y[0, 0]))
 print('Y[{},0] = {}\n'.format(-1, Y[-1, 0]))
 print("A = {}\n".format(A))
 
-J = np.sum((A*X-Y)**2)/(40)
+J = np.mean((A*X-Y)**2)/2
 print(J)
 
-J = (np.sum(A*X-Y)**2)/(20000)
+J = (np.mean(A*X-Y)**2)/(2000)
 print(J)
 
 K = (np.sum(A*(X**2)-(Y*X), axis=0)/20)
