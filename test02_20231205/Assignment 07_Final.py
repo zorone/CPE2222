a5ded4769761ce0363153cff2a5f06b891140679
Imron jehleh <imron.jehleh@gmail.com>
2023-12-17 03:03:51 +0700

    Write TODO list.

--- test02_20231205/Assignment 07_Final.py
+++ test02_20231205/Assignment 07_Final.py
@@ -1,7 +1,36 @@
-""" 
-    TODO:
-    HACK:
-    FIXME:
-    BUG:
-    UNDONE:
-"""
+# TODO: Write codes as Assigned.
+# ? Don't forget to add citation.
+
+# TODO: Navigate to current directory
+
+# TODO: Fetch Data
+
+# TODO: Read Data using numpy
+
+# TODO: Manipulated it
+
+# TODO: 1.
+# ? 1. Mean of monetary base of all February in Leap year.
+
+# TODO: 2.
+# ? 2. means for each year of 'Net Claims on Central Government'
+# * Pandas Series
+# * 1981 - 1990
+
+# TODO: 3.
+# ? 3. List of net loss continuously for 3 months
+# * 1991 - 2005
+
+# TODO: 4.
+# ? 4. List of 'Claims on Financial Institutions' is greater than 583757 for at least 3 month, In 6 month ranges
+# * 2000 - 2005
+# * 583757
+
+# TODO: 5.
+# ? 5. Tables of data, for min, mean, and max, of 'Other Liabilities to Financial Institutions' and 'Other Items (net)' for all year, based on each month.
+# * Pandas Data Frame
+# * In this Form:
+# * 'Other Liabilities to Financial Institutions': min, mean, max
+# * 'Other Items (net)': min, mean, max
+
+# TODO: Initialize program
