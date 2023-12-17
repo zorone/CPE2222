3db4036f6a4cc371c73cadc3d1dea63eeed5a0c4
Imron jehleh <imron.jehleh@gmail.com>
2023-12-18 05:54:02 +0700

    Now it's out of bound. That's really weird.

--- test02_20231205/Assignment 07_Final.py
+++ test02_20231205/Assignment 07_Final.py
@@ -123,7 +123,7 @@ class Main():
         # ? src: https://pandas.pydata.org/docs/user_guide/timeseries.html#dateoffset-objects
         
         self.dataByYear = self.data.groupby(self.data['time'].dt.year)
-        self._res_2 = self.dataByYear[self.dataKey[9]].mean().loc[1981:1990]
+        self._res_2 = self.dataByYear[self.dataKey[9]].loc[1981:1990].mean()
         print(self._res_2)
 
 # TODO: 3.
