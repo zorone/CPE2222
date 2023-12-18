ac72b8e600770a16246d071ff61284555714d236
Imron jehleh <imron.jehleh@gmail.com>
2023-12-19 02:35:06 +0700

    AssertionError, Huh.

--- test02_20231205/Assignment 07_Final.py
+++ test02_20231205/Assignment 07_Final.py
@@ -234,7 +234,7 @@ class Main():
         # * src: https://pandas.pydata.org/docs/dev/reference/api/pandas.Series.agg.html
         # * src: https://pandas.pydata.org/docs/dev/reference/api/pandas.DataFrame.agg.html
         
-        self.dataByMonth = self.data.groupby(self.data['time'].dt.month).agg([self.dataKey[24], self.dataKey[27]]: ['min', 'mean', 'max'])
+        self.dataByMonth = self.data.groupby(self.data['time'].dt.month).agg(self.dataKey[24], self.dataKey[27])
 
 # TODO: Implement pandas installation.
     def pdInstall(self):
