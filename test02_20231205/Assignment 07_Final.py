45d6cd1928c67e5a53eb76ab66b8cab209c5e44e
Imron jehleh <imron.jehleh@gmail.com>
2023-12-19 07:13:14 +0700

    Yeah, now it's back.

--- test02_20231205/Assignment 07_Final.py
+++ test02_20231205/Assignment 07_Final.py
@@ -232,9 +232,17 @@ class Main():
         # * src: https://pandas.pydata.org/docs/dev/reference/api/pandas.Series.agg.html
         # * src: https://pandas.pydata.org/docs/dev/reference/api/pandas.DataFrame.agg.html
         
+        # ? Further Reading
+        # ? src: https://stackoverflow.com/questions/60999753/pandas-future-warning-indexing-with-multiple-keys
+        # ? src: https://github.com/pandas-dev/pandas/issues/49247
+        # ? src: https://pandas.pydata.org/docs/whatsnew/v2.1.0.html#other-deprecations (GH 41090, GH 50684, GH 52849)
+        # ?     src-mention: https://github.com/pandas-dev/pandas/issues/41090
+        # ? --> src-mention: https://github.com/pandas-dev/pandas/issues/50684
+        # ?     src-mention: https://github.com/pandas-dev/pandas/issues/52849
+        
         # TODO: some more formatting.
         
-        self.dataByMonth = self.data.groupby(self.data['time'].dt.month, as_index=False).agg({[self.dataKey[24]]: ['min', 'mean', 'max'], [self.dataKey[27]]: ['min', 'mean', 'max']})
+        self.dataByMonth = self.data.groupby(self.data['time'].dt.month).agg({self.dataKey[24]: ['min', 'mean', 'max'], self.dataKey[27]: ['min', 'mean', 'max']})
         tempDictIndex = dict()
         tempDictColumn = dict()
         for i, name in enumerate(('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'), start=0):
